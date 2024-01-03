91775df2d37722b81a09cc720a73fba5d091668a
Eldar Bazhenov <104391118+bergemon@users.noreply.github.com>
2024-01-04 01:31:36 +0600

    Delete build directory

--- build/CMakeFiles/c_test_out.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for c_test_out.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bergemon/Документы/CPP/CMake Projects/c_test
-# Build directory: /home/bergemon/Документы/CPP/CMake Projects/c_test/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
