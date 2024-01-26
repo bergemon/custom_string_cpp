40233f2ca326d387c07473c0c7a76c6ff5d6b643
Эльдар Баженов <bergemon25@gmail.com>
2024-01-26 16:14:30 +0600

    added method find

--- include/custom_string.hpp
+++ include/custom_string.hpp
@@ -1,4 +1,4 @@
-#include "dependencies.hpp"
+#include <cstdint>
 
 namespace my_str {
     class string {
@@ -10,29 +10,35 @@ namespace my_str {
         // Constructors
         string() {}
         string(const char* str) {
+
             char currentChar = str[0];
-            while(currentChar != '\0')
-                currentChar = str[m_length++];
+            for (int i = 0;;++i) {
+                if (currentChar != '\0') {
+                    currentChar = str[i];
+                }
+                else {
+                    m_length = i;
+                    break;
+                }
+            }
 
             m_str = new char[m_length];
-            uint32_t i = m_length;
-            while(i > 0)
-                m_str[i] = str[--i];
+            for (int i = 0; i < m_length; ++i)
+                m_str[i] = str[i];
         }
         string(const string& str) {
-            m_length = str.length();
+            m_length = str.length() + 1;
             m_str = new char[m_length];
-            uint32_t i = m_length;
-            while(i > 0)
-                m_str[i] = str[--i];
+            for (int i = 0; i < m_length; ++i)
+                m_str[i] = str[i];
         }
         // Destructor
         ~string() { delete[] m_str; }
 
         // Getters
-        uint32_t length() const { return m_length; }
+        uint32_t length() const { return m_length - 1; }
         char* begin() const { return m_str; }
-        char* end() const { return m_str + m_length; }
+        char* end() const { return m_str + length(); }
 
         // Type conversation
         operator char*() const { return m_str; }
@@ -40,18 +46,18 @@ namespace my_str {
         // Operator+= with another string class
         string& operator+=(const string& str) {
             string tempStr = m_str;
-            delete[] m_str;
 
-            uint32_t accumLength = --m_length + str.length();
+            if (m_str != nullptr) { delete[] m_str; }
+            if (m_length > 0) { --m_length; }
+
+            uint32_t accumLength = m_length + (str.length() + 1);
             m_str = new char[accumLength];
 
-            uint32_t i = m_length;
-            while(i > 0)
-                m_str[i] = tempStr[--i];
+            for (int i = 0; i < m_length; ++i)
+                m_str[i] = tempStr[i];
 
-            i = accumLength;
-            while(i > m_length)
-                m_str[i] = str[--i - m_length];
+            for (int a = m_length, b = 0; a < accumLength; ++a, ++b)
+                m_str[a] = str[b];
 
             m_length = accumLength;
 
@@ -71,24 +77,56 @@ namespace my_str {
         string& operator=(const string& str) {
             delete[] m_str;
 
-            char currentChar = str[0];
-            m_length = 0;
-            while(currentChar != '\0')
-                currentChar = str[m_length++];
-
+            m_length = str.length() + 1;
             m_str = new char[m_length];
-            uint32_t i = m_length;
-            while(i > 0)
-                m_str[i] = str[--i];
+            for (int i = 0; i <= m_length; ++i) {
+                m_str[i] = str[i];
+            }
 
             return *this;
         }
+
         // Operator= with char pointer
-        string& operator=(const char* str) {
+        string& operator=(char* str) {
             delete[] m_str;
-            const string tempStr = str;
+            m_str = nullptr;
+            m_length = 0;
+            string tempStr = str;
 
             return *this += tempStr;
+        }
+
+        // Find substring in this string
+        int64_t find(const char* str) {
+            int64_t str_length = 0;
+            char checkSymbol;
+            for (; checkSymbol != '\0'; ++str_length) {
+                checkSymbol = str[str_length];
+            }
+
+            int64_t iter = 0;
+            int64_t substr_s = 0;
+
+            for (const char& elem : *this) {
+                if (elem == str[substr_s] && substr_s != str_length)
+                {
+                    ++substr_s;
+                }
+                else if (elem == str[substr_s] && substr_s == str_length)
+                {
+                    return iter;
+                }
+                else if (elem != str[substr_s] && substr_s > 0)
+                {
+                    substr_s = 0;
+                    if(elem == str[substr_s]) { ++substr_s; }
+                }
+                else { substr_s = 0; }
+
+                ++iter;
+            }
+
+            return -1;
         }
     };
 }
--- src/main.cxx
+++ src/main.cxx
@@ -1,15 +1,17 @@
 #include "../include/custom_string.hpp"
+#include "../include/dependencies.hpp"
 
 int main(int argc, char** argv) {
     using namespace my_str;
 
-    const string s1 = "Hello";
-    const string s2 = ", World!";
-    const string s3 = s1 + s2;
+    string s1 = "Hello";
+    string s2 = ", World!";
+    string s3 = s1 + s2;
     string s4 = "Hey! ";
     s4 += s3;
 
     std::cout << s4 << "\nlength: " << s4.length() << std::endl;
+    std::cout << s4.find("World") << std::endl;
 
     return 0;
 }
